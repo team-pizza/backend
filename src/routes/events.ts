0375ea70e22a11248807bc5ad91195352b2a3a5d
Austin <12504656+Austin-Scott@users.noreply.github.com>
2020-04-26 16:18:08 -0500

    Continue work on event upload endpoint

--- src/routes/events.ts
+++ src/routes/events.ts
@@ -2,10 +2,27 @@ import express from 'express'
 
 let router = express.Router()
 
+interface Event {
+    start: Date
+    durationInMinutes: Number
+}
+interface EventList {
+    events: Array<Event>
+    start: Date
+    spanInMinutes: Number
+}
+
 router.post('/upload', async (req, res, next) => {
-    console.log(req.header('sessionToken'))
-    console.log(req.body)
-    res.sendStatus(200)
+    let sessionToken = req.header('sessionToken')
+    // TODO: Use sessionToken to verify identity of who sent this
+
+    let eventList: EventList = req.body
+
+    // TODO: Add events into database
+
+    res.json({
+        success: true
+    })
 })
 
 export default router
