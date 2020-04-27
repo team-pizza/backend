3cbd96ac7cd966d39143e8315c710933589a24ae
Austin <12504656+Austin-Scott@users.noreply.github.com>
2020-04-27 14:15:01 -0500

    Use client secret as well for auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,7 +2,7 @@ import express from 'express'
 
 const cred = require('../../credentials.json')
 const { OAuth2Client } = require('google-auth-library')
-const client = new OAuth2Client(cred.web.client_id)
+const client = new OAuth2Client(cred.web.client_id, cred.web.client_secret)
 
 async function verify(token: String) {
     const ticket = await client.verifyIdToken({
